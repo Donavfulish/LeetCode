3fa3a6d6d269d239c97ce5f5a1def40ea33fc257
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-10-03 16:11:13 +0700

    Sliding window: 3

--- SlidingWindow/algomap.cpp
+++ SlidingWindow/algomap.cpp
@@ -21,4 +21,22 @@ public:
     }
 };
 
-//
+//3
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> lastIndex;
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            if (lastIndex.find(s[right]) != lastIndex.end() && lastIndex[s[right]] >= left) {
+                // Nếu gặp ký tự trùng trong cửa sổ hiện tại, dời left lên sau vị trí trùng
+                left = lastIndex[s[right]] + 1;
+            }
+            lastIndex[s[right]] = right; // cập nhật vị trí mới nhất của ký tự
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
