ef5edad2443c001198091152b5a92a255371a88a
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-10-05 17:22:31 +0700

    Tree: 110

--- Tree/algomap.cpp
+++ Tree/algomap.cpp
@@ -67,4 +67,35 @@ public:
     }
 };
 
-//
+// 110
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return check(root) != -1;
+    }
+    int check(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        
+        int leftD = check(root->left);
+        if (leftD == -1)
+            return -1;
+        int rightD = check(root->right);
+        if (rightD == -1)
+            return -1;
+        if (abs(leftD - rightD) > 1)
+            return -1;
+        return 1 + max(leftD, rightD);
+    }
+};
