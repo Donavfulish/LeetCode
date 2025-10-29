2559ac1425f15a077119f25af85c5125f641e90d
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-10-29 22:07:25 +0700

    base: 1.3

--- Array & String/leetcode.cpp
+++ Array & String/leetcode.cpp
@@ -5,15 +5,15 @@
 using namespace std;
 
 //----- Basic Problem: 1.1 -----------
-// bool isUnique(string s) {
-//     sort(s.begin(), s.end()); // O(NlogN)
-//     for (int i = 0; i < s.length() - 1; i++) { 
-//         if (s[i] == s[i + 1]) 
-//             return false;
-//     }
+bool isUnique(string s) {
+    sort(s.begin(), s.end()); // O(NlogN)
+    for (int i = 0; i < s.length() - 1; i++) { 
+        if (s[i] == s[i + 1]) 
+            return false;
+    }
 
-//     return true;
-// }
+    return true;
+}
 
 //----- Basic Problem: 1.2 -----------
 bool isPermutation(string s1, string s2) {
@@ -25,11 +25,36 @@ bool isPermutation(string s1, string s2) {
     return s1 == s2;
 }
 
+//----- Basic Problem: 1.3 -----------
+void URLify(char str[], int trueLength) {
+    // Count number of spaces in str
+    int numberSpace = 0;
+    for (int i = 0; i < trueLength; i++) {
+        if (str[i] == ' ') 
+            ++numberSpace;
+    }
+
+    // Each " " -> "%20" -> increase 2 character
+    int endPosition = trueLength + numberSpace * 2;
+    str[endPosition] = '\0';
+    for (int i = trueLength - 1; i >= 0; i--) {
+        if (str[i] == ' ') {
+            str[endPosition - 1] = '%';
+            str[endPosition - 2] = '2';
+            str[endPosition - 3] = '0';
+            endPosition -= 3;
+        } else {
+            str[endPosition - 1] = str[i];
+            endPosition -= 1;
+        }
+    }
+}
 // Relevant problem: 217, 242, 409, 3
 
 int main() {
-    string s1, s2;
-    cin >> s1 >> s2;
-    cout << isPermutation(s1, s2);
-    return 1;
+    char str[100] = "Mr John Smith    ";  // assume extra buffer
+    int trueLength = 13;
+    URLify(str, trueLength);
+    cout << str << endl; // Output: Mr%20John%20Smith
+    return 0;
 }
