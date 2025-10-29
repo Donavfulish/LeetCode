af5b6497c5020127aee97b1f421b53a8a98754b4
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-10-29 21:29:19 +0700

    base: 1.2

--- Array & String/leetcode.cpp
+++ Array & String/leetcode.cpp
@@ -1,9 +1,10 @@
-// Basic Problem: 1.1
-// #include <iostream>
-// #include <string>
-// #include <algorithm>
-// using namespace std;
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+//----- Basic Problem: 1.1 -----------
 // bool isUnique(string s) {
 //     sort(s.begin(), s.end()); // O(NlogN)
 //     for (int i = 0; i < s.length() - 1; i++) { 
@@ -14,11 +15,21 @@
 //     return true;
 // }
 
-// int main() {
-//     string s;
-//     cin >> s;
-//     cout << isUnique(s);
-//     return 1;
-// }
+//----- Basic Problem: 1.2 -----------
+bool isPermutation(string s1, string s2) {
+    if (s1.length() != s2.length()) 
+        return false;
+
+    sort(s1.begin(), s1.end()); // O(NlogN)
+    sort(s2.begin(), s2.end());
+    return s1 == s2;
+}
 
 // Relevant problem: 217, 242, 409, 3
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << isPermutation(s1, s2);
+    return 1;
+}
